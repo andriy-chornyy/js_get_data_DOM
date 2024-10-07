a70766587b16d6a0347d72b1ac5e787e0659ed21
Andrii Chornyi <ixxxx@i.ua>
2024-10-07 18:06:46 +0300

    make build

---
+++ index.92920af2.js
@@ -0,0 +1,2 @@
+var t=Array.from(document.querySelectorAll(".population")).map(function(t){return parseInt(t.textContent.replace(/,/g,""),10)}),e=t.reduce(function(t,e){return t+e},0),n=e/t.length,o=function(t){return t.toLocaleString()};document.querySelector(".total-population").textContent=o(e),document.querySelector(".average-population").textContent=o(Math.round(n));
+//# sourceMappingURL=index.92920af2.js.map
